0a429ec99e3dc655ff7f0a5ee1eca02f0684504f
Yan <yans@yancomm.net>
2016-03-15 18:40:47 -0700

    some fixes for longinit

--- tests_src/longinit.c
+++ tests_src/longinit.c
@@ -6,10 +6,10 @@ int main()
 	char a[10];
 	char b[10];
 
-	for (i = 0; i < 0x13371337; i++);
-	if (i == 0x13371337) scanf("%9s", a);
-	for (j = 0; j < 0xc001b347; i++);
-	if (j == 0xc001b347) scanf("%9s", b);
+	for (i = 0; i < 0x31337; i++);
+	if (i == 0x31337) scanf("%9s", a);
+	for (j = 0; j < 0xb3475; j++);
+	if (j == 0xb3475) scanf("%9s", b);
 
 	printf("You entered %s and %s!\n", a, b);
 }
