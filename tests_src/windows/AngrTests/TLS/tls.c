221ac93cd3d6bf0a78e6745e3cc96685ab64cd2f
Adrian Herrera <adrian.herrera02@gmail.com>
2016-07-20 11:57:54 +0200

    Improved Windows TLS sample
    * Disabled ASLR to make it easier to debug :-)
    * Modified the code to actually use multithreading, which is probably the best way to actually demonstrate TLS...

--- tests_src/windows/AngrTests/TLS/tls.c
+++ tests_src/windows/AngrTests/TLS/tls.c
@@ -1,5 +1,18 @@
 /*
- * Simple test application for Windows Thread Local Storage (TLS).
+ * Simple test application for Windows Thread Local Storage (TLS). It
+ * demonstrates both the use of TLS callbacks and TLS variables.
+ *
+ * Program flow:
+ *  1. When the main thread is created tls_callback is called and incrememnts
+ *     the TLS variable. In the main thread tls_i == 0xab
+ *  2. The main thread then creates myThread. Windows will then initialize a
+ *     new TLS data area for this thread. tls_i == 0xaa
+ *  3. tls_callback is called in myThread and increments tls_i. In myThread
+ *     tls_i == 0xab
+ *  4. myThread's entry point, thread_func, is then called and decrements
+ *     tls_i. tls_i == 0xaa. Note that in the main thread, tls_i == 0xab
+ *  5. thread_func returns and myThread is terminated. Its tls_i is
+ *     essentially deallocated
  */
 
 #include <Windows.h>
@@ -14,10 +27,19 @@ void NTAPI tls_callback(PVOID h, DWORD reason, PVOID reserved) {
 PIMAGE_TLS_CALLBACK p_thread_callback = tls_callback;
 #pragma data_seg()
 
+DWORD WINAPI thread_func(LPVOID lpParameter) {
+	tls_i--;
+
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
-	if (tls_i == 0xab) {
-		return 0;
-	} else {
-		return 1;
-	}
+	HANDLE myThread;
+
+	myThread = CreateThread(NULL, 0, thread_func, NULL, 0, NULL);
+	WaitForSingleObject(myThread, INFINITE);
+
+	// tls_i == 0xab in the main thread at this point in time
+
+	return 0;
 }
