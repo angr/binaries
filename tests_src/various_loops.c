6451628f69033188e0892aece51b22fc3879cec5
tyb0807 <sontuan.vu119@gmail.com>
2017-10-16 12:28:14 +0200

    Remove useless comments

--- tests_src/various_loops.c
+++ tests_src/various_loops.c
@@ -1,24 +1,3 @@
-/*
- * File Name : various_loops.c
- * Creation Date : 12-10-2017
- * Last Modified : lun. 16 oct. 2017 11:45:35 CEST
- * 
- * Copyright (C) Son Tuan Vu <son-tuan.vu@lip6.fr>
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <stdio.h>
 
 int a[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
