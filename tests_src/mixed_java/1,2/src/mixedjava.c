a9728e2dbe9089c37af3ac397a1e4cfb9395c70d
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-06-27 21:23:56 -0700

    Cleanup binaries for mixed java tests

--- tests_src/mixed_java/1,2/src/mixedjava.c
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include "mixedjava.h"
-
-JNIEXPORT jint JNICALL 
-Java_MixedJava_get_1version(JNIEnv *env, jclass cls){
-	jint version = (*env)->GetVersion(env);
-	return version;
-}
