69fd662d535c46dc3a12ae08e83b2c47ddc99b0b
Fish <fish@cs.ucsb.edu>
2020-06-25 12:36:34 -0700

    Add a new test function to string.c

--- tests_src/types/strings.c
+++ tests_src/types/strings.c
@@ -16,9 +16,31 @@ int local_strlen(const char *src)
 
 }
 
+char *local_strcat( char *dest, const char *src )
+{
+	int i = 0;
+	int j = 0;
+	int l;
+
+	if ( !dest ||  !src ) {
+		return dest;
+	}
+
+	i = local_strlen(dest);
+	l = local_strlen(src);
+
+	for( j = 0; j < l; i++, j++) {
+		dest[i] = src[j];
+	}
+
+	return dest;
+}
+
 int main()
 {
     int a = local_strlen("abcdef123456");
+    char b[1024] = {0}, *c = "abcd";
+    local_strcat(b, c);
     printf("a = %d\n", a);
     return 0;
 }
