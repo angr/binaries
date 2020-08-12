f517c1ae88f4a9ccf19f5c90ff1519827df0157c
Arvind <dnivra@users.noreply.github.com>
2020-08-12 11:24:16 -0700

    Enhanced unicorn stoptypes tests to support new stoptypes in unicorn engine (#59)

--- tests_src/uc_stop.c
+++ tests_src/uc_stop.c
@@ -2,7 +2,7 @@ void stop_normal() {
     for (int i = 0; i < 100; i++);
 }
 
-void stop_symbolic() {
+void stop_symbolic_read_symbolic_tracking_disabled() {
     int x[100];
     x[0] += 1;
 }
@@ -29,13 +29,35 @@ void stop_segfault() {
     ((void (*)())buf)();
 }
 
+void stop_symbolic_read_addr() {
+    long x;
+    int y = *(int *)x;
+}
+
+void stop_symbolic_write_addr() {
+    long x;
+    *(int *)x = 1;
+}
+
+void stop_symbolic_block_exit() {
+    int x[100];
+    x[0] += 1;
+    if (x[0] == 10) {
+        x[1] = 1;
+    }
+}
+
+void stop_symbolic_condition() {
+    // How to generate an VEX ITE expr with symbolic condition?
+}
+
 int main(int argc, char** argv) {
     switch (argc) {
         case 1:
             stop_normal();
             break;
         case 2:
-            stop_symbolic();
+            stop_symbolic_read_symbolic_tracking_disabled();
             break;
         case 3:
             stop_error();
@@ -51,6 +73,18 @@ int main(int argc, char** argv) {
             break;
         case 7:
             stop_segfault();
+            break;
+        case 8:
+            stop_symbolic_read_addr();
+            break;
+        case 9:
+            stop_symbolic_write_addr();
+            break;
+        case 10:
+            stop_symbolic_block_exit();
+            break;
+        case 11:
+            stop_symbolic_condition();
             break;
     }
 }
